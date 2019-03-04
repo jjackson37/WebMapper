1593032c857d48b3a9dda069ed627da649f4877e
Jamie Rudge <jamierudge93@gmail.com>
2019-03-04 14:14:54 +0000

    #2 Add validity check for TLD and refactor

--- WebMapper/PageRetrievers/WebClientPageRetriever.cs
+++ WebMapper/PageRetrievers/WebClientPageRetriever.cs
@@ -1,6 +1,7 @@
 ﻿namespace WebMapper.PageRetrievers
 {
     using System;
+    using System.Linq;
     using System.Net;
     using System.Text.RegularExpressions;
 
@@ -20,27 +21,51 @@
         public string RetrievePage(string url)
         {
             var htmlCode = string.Empty;
+            bool isValid = VerifyPageUrl(ref url);
 
-            // If user has not supplied http or https, prepend it
-            if (!Regex.IsMatch(url, "https?://"))
+            if (isValid)
             {
-                // TODO #3 : Check health of url and fix it
-                url = "http://" + url;
-            }
-
-            using (var client = new WebClient())
-            {
-                try
+                using (var client = new WebClient())
                 {
-                    htmlCode = client.DownloadString(url);
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine(e);
+                    try
+                    {
+                        htmlCode = client.DownloadString(url);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e);
+                    }
                 }
             }
 
             return htmlCode;
+        }
+
+        /// <summary>
+        /// Verifies the page URL.
+        /// </summary>
+        /// <param name="url">The URL.</param>
+        /// <returns>The validity of the url</returns>
+        private bool VerifyPageUrl(ref string url)
+        {
+            // TODO #2 : Move this once data solution is agreed on
+            var tempDomains = new string[] { ".co.uk", ".com", ".me", ".uk" };
+
+            // Check we have a top-level domain
+            if (!tempDomains.Any(url.Contains))
+            {
+                // TODO : Error Handle here.
+                return false;
+            }
+
+            // If user has not supplied http or https, prepend it
+            if (!Regex.IsMatch(url, "^https?://"))
+            {
+                // TODO #2 : Check health of url and fix it
+                url = "http://" + url;
+            }
+
+            return true;
         }
     }
 }
